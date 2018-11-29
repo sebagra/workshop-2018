b0f29b01f4545b5742a2db7383ffc1729dc28bc5
Sebastián Grattarola <sebagra@gmail.com>
2018-11-29 15:25:17 -0300

    Add open issues count to dashboard

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { issueStore } = require('../models');
 
 router.get('/', function(req, res) {
-  res.render('dashboard', {});
+  res.render('dashboard', {
+    openIssuesCount: issueStore.getAllOpen().length
+  });
 });
 
 module.exports = router;
